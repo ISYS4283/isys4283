10188581d3fe967854be312e59ca129829f51151
Jeff Puckett <jeff@jeffpuckett.com>
2017-02-08 17:23:34 +0000

    add resource links

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -6,10 +6,18 @@
 
 <ul>
     <li>
+        <a href="https://www.lynda.com/Programming-Foundations-tutorials/Welcome/412845/438411-4.html">Simon Allardice teaches <u>Foundations of Programming: Databases</u> on Lynda.com</a>. Get FREE access by just logging in with your UARK credentials.
+    </li>
+
+    <li>
         Unsigned Integers in
         <a href="http://stackoverflow.com/q/3895692/4233593" target="_blank">MySQL</a>
         vs
         <a href="http://dba.stackexchange.com/q/53050/92376" target="_blank">MSSQL</a>
+    </li>
+
+    <li>
+        <a href="https://www.udacity.com/course/how-to-use-git-and-github--ud775">How to Use Git and GitHub</a>. FREE course on udacity.
     </li>
 </ul>
 
