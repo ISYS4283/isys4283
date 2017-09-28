4a9e75908b034ce21447fdce5bc3dec2a0e0f411
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-28 15:11:46 +0000

    remove target="_blank"

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -39,43 +39,43 @@
     </li>
 
     <li>
-        <a href="https://serversforhackers.com/laravel-perf/mysql-indexing-two" target="_blank">
+        <a href="https://serversforhackers.com/laravel-perf/mysql-indexing-two">
             MySQL Indexing
         </a>
     </li>
 
     <li>
-        <a href="https://www.informit.com/articles/article.aspx?p=25862" target="_blank">
+        <a href="https://www.informit.com/articles/article.aspx?p=25862">
             The Cost of GUIDs as Primary Keys
         </a>
     </li>
 
     <li>
         Unsigned Integers in
-        <a href="http://stackoverflow.com/q/3895692/4233593" target="_blank">MySQL</a>
+        <a href="http://stackoverflow.com/q/3895692/4233593">MySQL</a>
         vs
-        <a href="http://dba.stackexchange.com/q/53050/92376" target="_blank">MSSQL</a>
+        <a href="http://dba.stackexchange.com/q/53050/92376">MSSQL</a>
     </li>
 
     <li>
         Geographic Data
         <ul>
-            <li><a href="https://gis.stackexchange.com/q/53918" target="_blank">
+            <li><a href="https://gis.stackexchange.com/q/53918">
                 Can one zip code span across multiple states?
             </a></li>
-            <li><a href="https://www.mjt.me.uk/posts/falsehoods-programmers-believe-about-addresses/" target="_blank">
+            <li><a href="https://www.mjt.me.uk/posts/falsehoods-programmers-believe-about-addresses/">
                 Falsehoods programmers believe about addresses
             </a></li>
-            <li><a href="http://stackoverflow.com/a/16743805/4233593" target="_blank">
+            <li><a href="http://stackoverflow.com/a/16743805/4233593">
                 What is the maximum length of latitude and longitude?
             </a></li>
-            <li><a href="http://stackoverflow.com/a/1196429/4233593" target="_blank">
+            <li><a href="http://stackoverflow.com/a/1196429/4233593">
                 What datatype to use when storing latitude and longitude data in SQL databases?
             </a></li>
-            <li><a href="https://en.wikipedia.org/wiki/Rhode_Island" target="_blank">
+            <li><a href="https://en.wikipedia.org/wiki/Rhode_Island">
                 What state has the longest name?
             </a></li>
-            <li><a href="https://en.wikipedia.org/wiki/Longest_word_in_English#Place_names" target="_blank">
+            <li><a href="https://en.wikipedia.org/wiki/Longest_word_in_English#Place_names">
                 What city has the longest name?
             </a></li>
         </ul>
